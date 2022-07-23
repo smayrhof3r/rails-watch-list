1d18aaa5f0cade0c8bd7a49a8b23ea30a8736aa8
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-07-23 13:54:19 +0100

    index page complete

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,17 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+
+top_movies_api = "https://tmdb.lewagon.com/movie/top_rated"
+top_movies = JSON.parse(URI.open(top_movies_api).read)["results"]
+top_movie_genres = top_movies.map { |movie| movie["genre_ids"] }
+                             .flatten
+
+genres_api = "https://tmdb.lewagon.com/genre/movie/list"
+genres = JSON.parse(URI.open(genres_api).read)["genres"]
+
+genres.filter! do |genre|
+  top_movie_genres.include?(genre["id"])
+end
+
+genres.each { |genre|
+  list = List.create(
+    name: genre["name"],
+  )
+  top_movies
+    .filter do |movie_data|
+      movie_data["genre_ids"].include?(genre["id"])
+    end
+    .each do |movie_data|
+      movie = Movie.create(
+        title: movie_data["title"],
+        overview: movie_data["overview"],
+        poster_url: movie_data["poster_path"],
+        rating: movie_data["vote_average"]
+      )
+      Bookmark.create(
+        movie_id: movie.id,
+        list_id: list.id,
+        comment: "#{genre["name"]} rocks!"
+      )
+    end
+}
